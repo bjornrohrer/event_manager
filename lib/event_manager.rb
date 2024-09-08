454e9cb743a3efef8fba6ce5412343a662d58655
bjornrohrer <barohrerdk@gmail.com>
2024-09-08 13:07:18 +0200

    Added google civicinfo and legislators.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,10 +1,14 @@
 require 'csv'
-puts "EventManager initialized"
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
 
+puts "EventManager initialized"
 
 contents = CSV.open(
   "event_attendees.csv",
@@ -14,6 +18,16 @@ contents = CSV.open(
 
 contents.each do |row|
   name = row[:first_name]
+
   zipcode = clean_zipcode(row[:zipcode])
-  puts "#{name} #{zipcode}"
+
+  legislators = civic_info.representative_info_by_address(
+    address: zipcode,
+    levels: 'country'
+    roles: ['legislatorUpperBody', 'legislatorLowerBody']
+  )
+
+  legislators = legislators.officials
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
