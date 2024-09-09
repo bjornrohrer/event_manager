0356e535e8d5555c4403b2aaf2c2ef45f41c9d14
bjornrohrer <barohrerdk@gmail.com>
2024-09-09 12:19:36 +0200

    Added the method.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -43,12 +43,12 @@ template_letter = File.read("../form_letter.html")
 erb_template = ERB.new template_letter
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-  
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
+  save_thank_you_letter(id, form_letter)
 end
